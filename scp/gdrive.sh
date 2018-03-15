a4eed6c364c231485b2012f2b0b22c96b6055349
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-03-15 12:21:11 -0700

    adcionando gdrive.sh,Thu Mar 15 15:20:52 EDT 2018

---
+++ scp/gdrive.sh
@@ -0,0 +1,22 @@
+wget https://doc-08-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/hdnb7d4912lhq7jsjn83ocq8fgucov3q/1521136800000/15876260727594163214/*/0B3X9GlR6EmbnQ0FtZmJJUXEyRTA
+mv 0B3X9GlR6EmbnQ0FtZmJJUXEyRTA gdrive
+chmod +x gdrive && mv gdrive /usr/bin
+
+#sudo add-apt-repository ppa:nilarimogard/webupd8
+#$ sudo apt-get update
+#$ sudo apt-get install grive
+
+
+
+#sudo apt-get install cmake build-essential libgcrypt11-dev libjson0-dev libcurl4-openssl-dev libexpat1-dev libboost-filesystem-dev libboost-program-options-dev binutils-dev
+#wget http://www.lbreda.com/grive/_media/packages/0.2.0/grive-0.2.0.tar.gz
+#tar xvfvz grive-0.2.0.tar.gz
+#cd grive-0.2.0
+#cmake .
+#make
+#sudo make install
+
+
+#mkdir ~/google_drive
+#cd ~/google_drive
+#grive -a

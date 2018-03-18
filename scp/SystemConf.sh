3fbb2b18d085927837a0d15538d12b3c61493def
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-03-18 03:52:01 -0700

    add SystemConfCentos,Sun Mar 18 06:51:55 EDT 2018

---
+++ scp/SystemConf.sh
@@ -0,0 +1,7 @@
+#permission denied
+sed -i '1s/^/x(){ chmod 777 -R \/home\/cabox\/workspace\n}\n\n/' /etc/bashrc
+
+#install sw
+sed -i '1s/^/\n\napp(){ yum install $* | tee $*.txt\n}\n\n/' /etc/bashrc
+
+. /etc/bashrc

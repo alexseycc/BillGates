69cbac029b51aca6f8f432c18bf1d34cc116cd69
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-03-25 14:58:17 -0700

    ,Sun Mar 25 17:58:16 EDT 2018

--- jquery.php
+++ jquery.php
@@ -13,9 +13,15 @@ $(".pa").click(function(){
 $(document).ready(function(){
 $("#pa").click(function(){
 $(this).css("color","red");
-})
-})
+})})
 
+  $(document).ready(function(){
+    $("#pa").css("color","blue");
+  $("#mm").click(function(){
+    alert("alan");
+    $(".pa").text("my name us");
+  });
+  });
 
 $(document).ready(function(){
 $("body").css("background-color","rgba(245,123,120,.4)");
@@ -24,13 +30,22 @@ $("body").css("background-color","rgba(245,123,120,.4)");
 window.onload = function() {
 alert("utilizando jquery");
 }
+
+
 </script>
 </head>
 <body>
 
 <p id="pa">If you click on me, I will disappear.</p>
 <p class="pa">Click me away!</p>
-<p>Click me too!</p>
-
-</body>
+<p id="mm">Click me too! <p/>
+<p id="nome" onclick="muda();">alex</p>
+<input type="text" id="txt">
+  </body>
 </html>
+<script>
+function muda(){
+  document.getElementById("nome").innerHTML="veka";
+  document.getElementById("txt").value="veka";
+}
+</script>

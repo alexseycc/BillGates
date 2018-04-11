b85eedf45ae58ff7ccbff81fa587c04c7ce5c767
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-04-11 09:31:13 -0700

    ,Wed Apr 11 12:31:11 EDT 2018

---
+++ python/interation.py
@@ -0,0 +1,25 @@
+#class clear:
+#def __call__(self):
+#  import os
+#  if os.name==('ce','nt','dos'): os.system('cls')
+#  elif os.name=='posix': os.system('clear')
+#  else: print('\n'*120)
+# def __neg__(self): self()
+# def __repr__(self):
+#  self();return ''
+
+#clear=clear()
+import os
+
+'''
+os.system('cls') # on windows
+or
+'''
+'kokokoko'
+i=0;
+while i < 10:
+  print "print ",i," times";
+  i=i+1;
+print "end";
+#clear();
+"os.system('clear')" 

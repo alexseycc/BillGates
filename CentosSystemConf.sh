ad4c5aead556c3d26f49588da4657cfff0a2f524
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-04-17 16:56:56 -0700

    ,Tue Apr 17 19:55:48 EDT 2018

--- CentosSystemConf.sh
+++ CentosSystemConf.sh
@@ -1,11 +1,21 @@
-sed -i '1s/^/#permission denied\nx(){ chmod 777 -R \/home\/cabox\/workspace\n}\n\n' /etc/bashrc
+#sed -i '1s/^/#permission denied\nx(){ chmod 777 -R \/home\/cabox\/workspace\n}\n\n' /etc/bashrc
 
-echo "#sw  instalation
+
+
+echo "
+#permission denied
+x(){ 
+chmod 777 -R /home/cabox/workspace
+}
+
+#sw  instalation
 app(){ 
 yum install $* | tee ./pckgLinux/$*.txt
 }
 
-`cat /etc/bashrc`" > /etc/bashrc
-. /etc/bashrc
+`cat /etc/bash.bashrc`" > /etc/bash.bashrc
+. /etc/bash.bashrc
+if [ -f pckgLinux ]; then
 mkdir ./pckgLinux/
+fi
 #sudo yum install java-1.7.0-openjdk.x86_64 
--- bin.sh
+++ bin.sh
@@ -1,4 +1,4 @@
 svn --force add *
 svn --force-log commit -m "$*,`date`"
-sh atualizadorFiles.sh
-megacopy -l /home/cabox/workspace/billgates.git/trunk -r /Root/billgates.git/trunk 
+#sh atualizadorFiles.sh
+#megacopy -l /home/cabox/workspace/billgates.git/trunk -r /Root/billgates.git/trunk 

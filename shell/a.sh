e238cb93be267e1daf1eaec4e6c10fa92b131bb8
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-04-18 09:19:29 -0300

    ,Wed Apr 18 08:19:28 EDT 2018

--- shell/a.sh
+++ shell/a.sh
@@ -1,5 +1,13 @@
-case $* in 
-ubuntu)
-echo vc escolheu $*
+#if [ "$*" == "root" || "$*" == "bob" ]; then
+# echo "$*"
+#fi
+
+#if [ -f test ]; then
+#echo diretorio existe
+#fi
+
+case "$1" in
+ubuntu | debian)
+echo alx
 ;;
-esac;
+esac

13720a9a10442cd72efed3ade6f69d239840cfe2
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-05-02 14:43:18 -0300

    add android,Wed May  2 13:43:16 EDT 2018

--- projetoWeb/android.sh
+++ projetoWeb/android.sh
@@ -1,1 +1,15 @@
-echo android
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+
+
+
+https://dl.google.com/dl/android/studio/ide-zips/3.1.2.0/android-studio-ide-173.4720617-linux.zip
+
+#https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+
+sudo unzip android-studio-ide-141.2178183-linux.zip -d /opt
+cd /opt/android-studio/bin
+#sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
+. ./studio.sh

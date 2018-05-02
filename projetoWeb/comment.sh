60edc4113511562670a6bc0ef185352623f34221
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-05-02 15:27:45 -0300

    ,Wed May  2 14:27:44 EDT 2018

---
+++ projetoWeb/comment.sh
@@ -0,0 +1,8 @@
+<<alx
+  Master LDAP server : dir1.nixcraft.net.in 
+    Add user to master and it will get sync to backup server too
+    Profile and active directory hooks are below
+sls
+lsl
+alx
+echo welcome

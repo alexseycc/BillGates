8d6e889e0806437e9960b1e38fab15a1220522d1
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-05-08 20:39:11 -0300

    ,Tue May  8 19:39:08 EDT 2018

---
+++ projetoWeb/atualizador.sh
@@ -0,0 +1,19 @@
+
+#sudo apt-get install python-software-properties
+echo "Dpkg::Options {
+--force-confnew
+#--force-confold
+#--force-confdef
+}
+" > /etc/apt/apt.conf.d/local
+
+
+
+< '
+up(){
+apt-get update -y
+#apt-get upgrade -y
+#sudo apt-get dist-upgrade -y
+apt-get install nano -y
+}
+'
---
+++ projetoWeb/java.sh
@@ -0,0 +1,10 @@
+sh atualizador.sh
+up
+
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get install oracle-java8-installer
+#sudo apt-get install oracle-java8-set-default
+
+
+sudo apt-get install default-jdk
+#sudo apt-get install oracle-java7-installer

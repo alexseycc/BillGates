d35b5a5aabdc34fdac2e14fe57acd46eaa2ebfc2
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-05-14 10:03:39 -0300

    ,Mon May 14 09:03:38 EDT 2018

--- 11_05_18.php
+++ 11_05_18.php
@@ -1,24 +1,23 @@
+<form action="#" method="get">
+    <input type="text" name="nome" />
+    <input type="submit" />
+</form>
+
 <?php
-$nome=htmlspecialchars($_GET["nome"]);
+
+if(!empty($_GET) && $_SERVER['REQUEST_METHOD'] == 'GET'){
+    $nome = $_GET['nome'];
+    echo 'valor: '.$nome.' e o valor do calculo(quadrado) eh:'.quadrado($_GET['nome']);
+echo "<br><script>alert(".quadrado($nome).")</script>";
+  $a="5";
+  $b=5;
+  echo $a==$b;
+}   
+
+function quadrado($x){
+return $x*$x;  
+  }
+
+
+
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Document</title>
-</head>
-<body>
-  <?php
-  for($i=0;$i<=4;$i++)
-    echo $i."<br>";
-  ?>
-<form action="11_05_18.php" method="GET">
-  <input type="text" name="nome"> </input>
-  <input type="submit" value="enviando"></input>
-  </form>
-  
-  
-  </body>
-</html>

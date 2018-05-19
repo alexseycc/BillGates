0176dfa14634db34d8211463e05d60da345f1de6
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-05-19 15:56:26 -0300

    ,Sat May 19 14:56:23 EDT 2018

--- projetoWeb/netbeans.sh
+++ projetoWeb/netbeans.sh
@@ -4,6 +4,6 @@ sh java.sh
 #sudo apt-get update -y
 #sudo apt-get install oracle-java8-installer -y
 #curl -O http://download.netbeans.org/netbeans/8.0/final/bundles/netbeans-8.0-linux.sh
-wget -c --no-check-certificate https://dl.dropboxusercontent.com/content_link/VZQpQer2lmJ0eILRY2jxRrDt2q2PXuA8FPjb3MenhvUHjeE4wyscoQAlqkniNR1P/file?_download_id=176546459817299930347610339937584567393199849785550321128821858&_notify_domain=www.dropbox.com&dl=1
+wget --no-check-certificate -c https://www.dropbox.com/s/uadiiguv19tu9r9/netbeans-8.0-linux.sh
 file=`http://download.netbeans.org/netbeans/8.0/final/bundles/netbeans-8.0-linux.sh | rev | cut -d/ -f1 | rev`
 sh $file
